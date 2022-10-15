76aad5bcb6ee61306ecccc99283d43e858aa79b1
pabloverat-macincloud <pvt-2000@hotmail.com>
2022-10-15 17:56:18 -0500

    arrange assets, added VC classes

---
+++ theApp/theApp/TableroViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TableroViewController.swift
+//  theApp
+//
+//  Created by user223248 on 10/15/22.
+//
+
+import UIKit
+
+class TableroViewController: UIViewController {
+
+    @IBOutlet weak var butt: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        butt.contentHorizontalAlignment = .fill
+        butt.contentVerticalAlignment = .fill
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
