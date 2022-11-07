2a1ae345637acf34368505a5349384edac1a0880
pabloverat-macincloud <pvt-2000@hotmail.com>
2022-11-07 15:04:44 -0600

    constraints tested on iphone Se, 14, ipad 10

--- theApp/theApp/TableroViewController.swift
+++ theApp/theApp/TableroViewController.swift
@@ -13,6 +13,10 @@ class TableroViewController: UIViewController {
     @IBOutlet weak var pistas: UIStackView!
     @IBOutlet weak var colores: UIStackView!
     
+    var marble : [Int:String] = [1:"green", 2:"aqua", 3:"orange", 4:"red", 5:"purple", 6:"yellow"]
+    
+    var guessEval : [Int:String] = [-1:"badGuess", 0:"mediumGuess", 1:"goodGuess"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         jugadas.layer.cornerRadius = 10
@@ -20,6 +24,38 @@ class TableroViewController: UIViewController {
         colores.layer.cornerRadius = 10
 
         // Do any additional setup after loading the view.
+        let code = generateCode()
+        print(code)
+        let guess = generateCode()
+        print(guess)
+
+        let evaluation = evaluateGuess(guess: guess, code: code)
+        print(evaluation)
+
+        
+    }
+    
+    func generateCode() -> [Int] {
+        var code = [Int]()
+        while(code.count < 4) {
+            let num : Int = Int.random(in: 1...6)
+            if (!code.contains(num)) {
+                code.append(num)
+            }
+        }
+        return code
+    }
+    
+    func evaluateGuess(guess : [Int], code : [Int]) -> [Int] {
+        var evaluation = [Int](repeating: -1, count: 4)
+        for i in 0...guess.count-1 {
+            if guess[i] == code[i]{
+                evaluation[i] = 1
+            } else if (code.contains(guess[i])) {
+                evaluation[i] = 0
+            }
+        }
+        return evaluation.sorted(by: >)
     }
     
 
